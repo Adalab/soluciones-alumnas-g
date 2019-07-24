8f305163254c466f166a39fe01c9dc2f2d315715
martagrio <marta.igr90@gmail.com>
2019-07-24 22:45:37 +0200

    Add exercise 6 from 2.5

--- modulo2/ej01/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const button = document.querySelector('.btn');
-const texto = document.querySelector('.text');
-
-function changeText() {
-    texto.innerHTML = "Mi primer click, ¡ole yo y la mujer que me parió!";
-}
-
-button.addEventListener('click', changeText);
