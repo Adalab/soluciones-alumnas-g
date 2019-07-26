937ef31643e8ef4c37af9b1903b381cd67f602ba
mariamata <matah.maria@gmail.com>
2019-07-26 16:01:55 +0200

    initial commit and full exercise

---
+++ modulo2/s2.7/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+let startYear = 2017;
+
+for (let i= 0; i< 15; i++){
+    startYear = startYear + 3;
+    console.log(startYear);
+}
+
+/*Cada 3 años se produce una luna llena completamente iluminada por el Sol durante unos minutos. 
+Esta luna es conocida como la “Luna del cazador”. 
+En el año 2017 se pudo ver esta luna el 5 de octubre y mucha gente se la perdió. 
+Para que no nos pase los siguientes años vamos a crear un código que muestre en consola cuándo serán las 15 próximas lunas.
+NOTA: Vamos a realizar este ejercicio de forma que, antes de programar nada, escribamos el un papel el listado de las acciones 
+(algoritmo) que tenemos que realizar para conseguir el resultado que buscamos. Una vez escrito este listado, ya nos pondremos a programarlo en JS. 
+*/
